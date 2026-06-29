0eceb5fba44d9ba4522898c9e29909253cfadbe8
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-06-29 22:40:52 +0100

    Update files.js

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,1 +1,31 @@
+const express = require("express");
 
+const router = express.Router();
+
+/* ========================================
+Get Files
+======================================== */
+
+router.get("/", (req, res) => {
+
+res.json({
+success: true,
+files: []
+});
+
+});
+
+/* ========================================
+Upload File
+======================================== */
+
+router.post("/upload", (req, res) => {
+
+res.json({
+success: true,
+message: "File Uploaded"
+});
+
+});
+
+module.exports = router;
