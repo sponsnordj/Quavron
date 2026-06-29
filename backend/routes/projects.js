e443f487d9c72f5eca9d3d64f43148269e6d4b82
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-06-29 22:40:14 +0100

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,1 +1,31 @@
+const express = require("express");
 
+const router = express.Router();
+
+/* ========================================
+Get Projects
+======================================== */
+
+router.get("/", (req, res) => {
+
+res.json({
+success: true,
+projects: []
+});
+
+});
+
+/* ========================================
+Create Project
+======================================== */
+
+router.post("/create", (req, res) => {
+
+res.json({
+success: true,
+message: "Project Created"
+});
+
+});
+
+module.exports = router;
