9f8ebfea46ad95b4970edf97d7dd8bbf8dfb29d9
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-06-29 22:31:28 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,52 @@
 
+console.log("🧠 Quavron IDE Loaded");
+
+/* ========================================
+Fake File Explorer System
+======================================== */
+
+const fileItems = document.querySelectorAll(".file-item");
+
+fileItems.forEach(file => {
+
+file.addEventListener("click", () => {
+
+fileItems.forEach(item => {
+  item.classList.remove("active-file");
+});
+
+file.classList.add("active-file");
+
+console.log("Opened:", file.innerText);
+
+});
+
+});
+
+/* ========================================
+Fake Run Button
+======================================== */
+
+const buttons = document.querySelectorAll(".editor-topbar button");
+
+buttons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+console.log(button.innerText + " Clicked");
+
+});
+
+});
+
+/* ========================================
+Monaco Editor Placeholder
+======================================== */
+
+function initializeEditor() {
+
+console.log("Monaco Editor Will Initialize Here");
+
+}
+
+initializeEditor();
